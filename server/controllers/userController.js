5361885671edca3d00dd8b3423b77ba554f5a32b
Ramesh Jayawickrama <jayawickramaramesh@gmail.com>
2025-03-11 08:59:49 +0530

    fixs

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -18,7 +18,7 @@ async function createUser(req, res) {
 async function getAllUsers(req, res) {
     try {
       const users = await User.find();
-      console.log(users); // Log the users to see the actual data structure
+      console.log(users); 
       res.status(200).json(users);
     } catch (error) {
       res.status(400).json({ message: error.message });
